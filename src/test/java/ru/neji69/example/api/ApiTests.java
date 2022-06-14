ed0f5f127583f6b72579194591891058c43bf89e
Ленчевский Ян Дмитриевич <Lenchevskiy.Ya.D@sberbank.ru>
2022-06-14 17:45:10 +0300

    Ver0.001

--- src/test/java/ru/neji69/example/api/ApiTests.java
+++ src/test/java/ru/neji69/example/api/ApiTests.java
@@ -1,4 +1,20 @@
 package ru.neji69.example.api;
 
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ApiTests {
+
+    private final String BASE_URL = "https://petstore.swagger.io/";
+    private final String PETS_PATH = "v2/pet/";
+
+    private RequestSpecification requestSpec;
+
+    @BeforeEach
+    void setUp(){
+        requestSpec = new 
+    }
+
+
+
 }
--- src/test/java/ru/neji69/example/api/pojo/pet/Pet.java
+++ src/test/java/ru/neji69/example/api/pojo/pet/Pet.java
@@ -4,10 +4,69 @@ import java.util.List;
 
 public class Pet {
 
+    public Pet(Integer id, Category category, String name, List<String> photoUrls, List<Tag> tags, Status status) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+        this.photoUrls = photoUrls;
+        this.tags = tags;
+        this.status = status;
+    }
+
     private Integer id;
     private Category category;
     private String name;
     private List<String> photoUrls;
     private List<Tag> tags;
     private Status status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
+
+
